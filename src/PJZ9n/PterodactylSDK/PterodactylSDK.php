041b0535e4f78cab591a289c4a1575885c0316d3
PJZ9n <pjz9n@yahoo.co.jp>
2020-03-08 08:30:48 +0900

    Create PterodactylSDK.php

---
+++ src/PJZ9n/PterodactylSDK/PterodactylSDK.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Copyright (c) 2020 PJZ9n.
+ *
+ * This file is part of Pterodactyl-PHP-SDK.
+ *
+ * Pterodactyl-PHP-SDK is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Pterodactyl-PHP-SDK is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Pterodactyl-PHP-SDK.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+declare(strict_types=1);
+
+namespace PJZ9n\PterodactylSDK;
+
+/**
+ * Class PterodactylSDK
+ *
+ * @package PJZ9n\PterodactylSDK
+ */
+abstract class PterodactylSDK
+{
+    
+    /**
+     * @var string API URL
+     * ex: https://pterodactyl.app/api
+     */
+    protected $apiUrl;
+    
+    /**
+     * @var string API KEY
+     * ex: HogeFuga12345abc
+     */
+    protected $apiKey;
+    
+    /**
+     * PterodactylSDK constructor.
+     *
+     * @param string $apiUrl API URL
+     * @param string $apiKey API KEY
+     */
+    protected function __construct(string $apiUrl, string $apiKey)
+    {
+        $this->apiUrl = $apiUrl;
+        $this->apiKey = $apiKey;
+    }
+    
+}
